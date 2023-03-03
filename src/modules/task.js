6c9cf51b642489c9daa5e670522411d174affc6c
mdg0410 <levoyersteven@gmail.com>
2023-03-03 11:50:16 -0500

    Add list task & Configure webpack

---
+++ src/modules/task.js
@@ -0,0 +1,23 @@
+export default class Task {
+  constructor(description, completed, index = null) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+
+  createNode() {
+    const taskNode = document.createElement('li');
+    taskNode.setAttribute('data-task', true);
+    taskNode.innerHTML = `
+    <div class="task-item-container">
+      <label htmlFor="complete-task" class="task-status"><input ${this.completed ? 'checked' : ''} type="checkbox" name="complete-task"/>${!this.completed ? `<i class="bi bi-square" data-task-id="${this.index}"></i>` : `<i class="bi bi-check-lg" data-task-id="${this.index}"></i>`} </label>
+      <form name="task-${this.index}">
+        <input type="text" class="task-item ${this.completed ? 'task-completed' : ''}" required name="description" data-task-id="${this.index}" value="${this.description}" />
+        <input type="hidden" value="${this.index}" name="index" />
+      </form>
+    </div>
+    <i class="fa-solid fa-trash hidden" data-task-index="${this.index}"></i><i class="fa-solid fa-bars" data-task-index="${this.index}"></i>    `;
+
+    return { taskNode, taskIndex: this.index };
+  }
+}
